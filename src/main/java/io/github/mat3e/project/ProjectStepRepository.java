6fa958fad63c45a38052852a2c706f8683f0d523
Cezary Sanecki <devcezz@gmail.com>
2021-02-28 00:12:33 +0100

    Made ProjectStepRepository to be package accessed

--- src/main/java/io/github/mat3e/project/ProjectStepRepository.java
+++ src/main/java/io/github/mat3e/project/ProjectStepRepository.java
@@ -2,5 +2,5 @@ package io.github.mat3e.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectStepRepository extends JpaRepository<ProjectStep, Integer> {
+interface ProjectStepRepository extends JpaRepository<ProjectStep, Integer> {
 }
