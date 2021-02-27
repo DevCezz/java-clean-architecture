fccbcaf2cdd890d15438e71380ae11565c033138
Cezary Sanecki <devcezz@gmail.com>
2021-02-27 22:45:18 +0100

    Expanded imports in Task entity

--- src/main/java/io/github/mat3e/entity/Task.java
+++ src/main/java/io/github/mat3e/entity/Task.java
@@ -2,7 +2,12 @@ package io.github.mat3e.entity;
 
 import org.springframework.data.annotation.PersistenceConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.ZonedDateTime;
 
