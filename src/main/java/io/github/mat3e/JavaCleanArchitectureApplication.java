2571e7cc4cbbf45c61bad08a184bafffdcdcb4a5
Cezary Sanecki <devcezz@gmail.com>
2021-02-28 13:16:38 +0100

    Deleted inserting project data from main class

--- src/main/java/io/github/mat3e/JavaCleanArchitectureApplication.java
+++ src/main/java/io/github/mat3e/JavaCleanArchitectureApplication.java
@@ -1,9 +1,7 @@
 package io.github.mat3e;
 
-import io.github.mat3e.project.Project;
-import io.github.mat3e.project.ProjectStep;
-import io.github.mat3e.task.Task;
 import io.github.mat3e.project.ProjectRepository;
+import io.github.mat3e.task.Task;
 import io.github.mat3e.task.TaskRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,24 +16,15 @@ public class JavaCleanArchitectureApplication implements ApplicationListener<Con
         SpringApplication.run(JavaCleanArchitectureApplication.class, args);
     }
 
-    private final ProjectRepository projectRepository;
     private final TaskRepository taskRepository;
 
     public JavaCleanArchitectureApplication(ProjectRepository projectRepository, TaskRepository taskRepository) {
-        this.projectRepository = projectRepository;
         this.taskRepository = taskRepository;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        if (projectRepository.count() == 0) {
-            var project = new Project();
-            project.setName("Example project");
-            project.addStep(new ProjectStep("First", -3, project));
-            project.addStep(new ProjectStep("Second", -2, project));
-            project.addStep(new ProjectStep("Third", 0, project));
-            projectRepository.save(project);
-        }
+
         if (taskRepository.count() == 0) {
             var task = new Task("Example task", ZonedDateTime.now(), null);
             taskRepository.save(task);
