041d57784f3a6b63fe5e161c51adc5403b47d3ce
Cezary Sanecki <devcezz@gmail.com>
2021-02-28 23:31:55 +0100

    Made TaskRepository to be package private

--- src/main/java/io/github/mat3e/task/TaskRepository.java
+++ src/main/java/io/github/mat3e/task/TaskRepository.java
@@ -2,6 +2,6 @@ package io.github.mat3e.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+interface TaskRepository extends JpaRepository<Task, Integer> {
     boolean existsByDoneIsFalseAndProject_Id(int id);
 }
