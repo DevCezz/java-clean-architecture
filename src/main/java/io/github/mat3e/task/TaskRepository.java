8d2f43e2b750a5cd6cfc6b404401fb221666ab0d
Cezary Sanecki <devcezz@gmail.com>
2021-03-01 12:42:51 +0100

    Changed JpaRepository to Repository in TaskRepository

--- src/main/java/io/github/mat3e/task/TaskRepository.java
+++ src/main/java/io/github/mat3e/task/TaskRepository.java
@@ -1,7 +1,19 @@
 package io.github.mat3e.task;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-interface TaskRepository extends JpaRepository<Task, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+interface TaskRepository extends Repository<Task, Integer> {
+
+    Optional<Task> findById(Integer id);
+
+    <S extends Task> S save(S entity);
+
+    <S extends Task> List<S> saveAll(Iterable<S> entities);
+
+    void deleteById(Integer id);
+
     boolean existsByDoneIsFalseAndProject_Id(int id);
 }
