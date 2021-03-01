d135c13bca1a83c9a1644a899232dcd511e9a069
Cezary Sanecki <devcezz@gmail.com>
2021-03-01 15:38:41 +0100

    Added inner class DeserializationImpl for deserialization ProjectDto

--- src/main/java/io/github/mat3e/project/dto/ProjectDto.java
+++ src/main/java/io/github/mat3e/project/dto/ProjectDto.java
@@ -9,4 +9,32 @@ public interface ProjectDto {
     String getName();
 
     List<ProjectStepDto> getSteps();
+
+    class DeserializationImpl implements ProjectDto {
+
+        private final int id;
+        private final String name;
+        private final List<ProjectStepDto> steps;
+
+        DeserializationImpl(final int id, final String name, final List<ProjectStepDto> steps) {
+            this.id = id;
+            this.name = name;
+            this.steps = steps;
+        }
+
+        @Override
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public List<ProjectStepDto> getSteps() {
+            return steps;
+        }
+    }
 }
