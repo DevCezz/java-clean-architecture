172f99c53db97a1a64c4f765c68b645f4c883538
Cezary Sanecki <devcezz@gmail.com>
2021-03-01 14:02:00 +0100

    Added required save method to ProjectRepository

--- src/main/java/io/github/mat3e/project/ProjectRepository.java
+++ src/main/java/io/github/mat3e/project/ProjectRepository.java
@@ -2,5 +2,11 @@ package io.github.mat3e.project;
 
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 interface ProjectRepository extends Repository<Project, Integer> {
+
+    Optional<Project> findById(int id);
+
+    <S extends Project> S save(S entity);
 }
