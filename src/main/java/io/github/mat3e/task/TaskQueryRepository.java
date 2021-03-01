9e4148d694ab57fa0ddce465d8b103cb06d44fe5
Cezary Sanecki <devcezz@gmail.com>
2021-03-01 13:29:03 +0100

    Changed return type to return list of TaskDto objects

--- src/main/java/io/github/mat3e/task/TaskQueryRepository.java
+++ src/main/java/io/github/mat3e/task/TaskQueryRepository.java
@@ -1,5 +1,6 @@
 package io.github.mat3e.task;
 
+import io.github.mat3e.task.dto.TaskDto;
 import io.github.mat3e.task.dto.TaskWithChangesDto;
 import org.springframework.data.repository.Repository;
 
@@ -9,7 +10,7 @@ public interface TaskQueryRepository extends Repository<Task, Integer> {
 
     int count();
 
-    List<Task> findAll();
+    List<TaskDto> findAllBy();
 
     boolean existsByDoneIsFalseAndProject_Id(int id);
 
