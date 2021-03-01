bf5843868f118f0b72aa409805c4e7526ffdd74f
Cezary Sanecki <devcezz@gmail.com>
2021-03-01 13:20:04 +0100

    Used proper method to check if exists undone tasks for project

--- src/main/java/io/github/mat3e/project/ProjectFacade.java
+++ src/main/java/io/github/mat3e/project/ProjectFacade.java
@@ -97,7 +97,7 @@ public class ProjectFacade {
     }
 
     List<TaskDto> createTasks(int projectId, ZonedDateTime projectDeadline) {
-        if (taskFacade.areUndoneTasksWithProjectId(projectId)) {
+        if (taskQueryRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
             throw new IllegalStateException("There are still some undone tasks from a previous project instance!");
         }
         return projectRepository.findById(projectId)
