1ba3c2366946176ee1a1464aaab857ebaaeeca49
Cezary Sanecki <devcezz@gmail.com>
2021-03-01 15:29:40 +0100

    Added inner class to deserialization ProjectStepDto

--- src/main/java/io/github/mat3e/project/dto/ProjectStepDto.java
+++ src/main/java/io/github/mat3e/project/dto/ProjectStepDto.java
@@ -7,4 +7,32 @@ public interface ProjectStepDto {
     String getDescription();
 
     int getDaysToProjectDeadline();
+
+    class DeserializationImpl implements ProjectStepDto {
+
+        private final int id;
+        private final String description;
+        private final int daysToProjectDeadline;
+
+        public DeserializationImpl(final int id, final String description, final int daysToProjectDeadline) {
+            this.id = id;
+            this.description = description;
+            this.daysToProjectDeadline = daysToProjectDeadline;
+        }
+
+        @Override
+        public int getId() {
+            return 0;
+        }
+
+        @Override
+        public String getDescription() {
+            return null;
+        }
+
+        @Override
+        public int getDaysToProjectDeadline() {
+            return 0;
+        }
+    }
 }
