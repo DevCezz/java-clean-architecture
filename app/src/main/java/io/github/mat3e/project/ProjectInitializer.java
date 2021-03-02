4b9490559a8e1e4b1a748cf396d1428bf6d66b76
Cezary Sanecki <devcezz@gmail.com>
2021-03-02 23:59:02 +0100

    Added required field int ProjectInitializer

--- app/src/main/java/io/github/mat3e/project/ProjectInitializer.java
+++ app/src/main/java/io/github/mat3e/project/ProjectInitializer.java
@@ -1,4 +1,12 @@
 package io.github.mat3e.project;
 
 class ProjectInitializer {
+
+    private final ProjectRepository projectRepository;
+    private final ProjectQueryRepository projectQueryRepository;
+
+    ProjectInitializer(final ProjectRepository projectRepository, final ProjectQueryRepository projectQueryRepository) {
+        this.projectRepository = projectRepository;
+        this.projectQueryRepository = projectQueryRepository;
+    }
 }
