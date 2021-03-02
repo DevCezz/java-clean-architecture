6cf3695b63d77ead622033c3c3f668d51b66cd51
Cezary Sanecki <devcezz@gmail.com>
2021-03-02 23:24:13 +0100

    Removed Repository interface to be used in ProjectQueryRepository

--- app/src/main/java/io/github/mat3e/project/ProjectQueryRepository.java
+++ app/src/main/java/io/github/mat3e/project/ProjectQueryRepository.java
@@ -1,12 +1,11 @@
 package io.github.mat3e.project;
 
 import io.github.mat3e.project.dto.ProjectDto;
-import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ProjectQueryRepository extends Repository<Project, Integer> {
+public interface ProjectQueryRepository {
 
     int count();
 
