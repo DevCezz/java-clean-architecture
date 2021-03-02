c51335e7c5c639db6ebb267034a4157544869dea
Cezary Sanecki <devcezz@gmail.com>
2021-03-02 22:17:27 +0100

    Deleted Spring Repository dependency in TaskQueryRepository

--- app/src/main/java/io/github/mat3e/task/TaskQueryRepository.java
+++ app/src/main/java/io/github/mat3e/task/TaskQueryRepository.java
@@ -2,12 +2,11 @@ package io.github.mat3e.task;
 
 import io.github.mat3e.task.dto.TaskDto;
 import io.github.mat3e.task.dto.TaskWithChangesDto;
-import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface TaskQueryRepository extends Repository<Task, Integer> {
+public interface TaskQueryRepository {
 
     int count();
 
