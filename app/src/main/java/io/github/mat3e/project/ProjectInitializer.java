f58272ce6d9969bed5c83b426c442dd2a52728ef
Cezary Sanecki <devcezz@gmail.com>
2021-03-02 23:59:22 +0100

    Added init method to initialize project data

--- app/src/main/java/io/github/mat3e/project/ProjectInitializer.java
+++ app/src/main/java/io/github/mat3e/project/ProjectInitializer.java
@@ -9,4 +9,15 @@ class ProjectInitializer {
         this.projectRepository = projectRepository;
         this.projectQueryRepository = projectQueryRepository;
     }
+
+    void init() {
+        if (projectQueryRepository.count() == 0) {
+            var project = new Project();
+            project.setName("Example project");
+            project.addStep(new ProjectStep("First", -3, project));
+            project.addStep(new ProjectStep("Second", -2, project));
+            project.addStep(new ProjectStep("Third", 0, project));
+            projectRepository.save(project);
+        }
+    }
 }
