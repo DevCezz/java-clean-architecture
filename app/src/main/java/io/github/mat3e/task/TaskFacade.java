14c28c1a2ff2b43cfe8d30a1c20c2bb08d805205
Cezary Sanecki <devcezz@gmail.com>
2021-03-02 23:56:13 +0100

    Removed unnecessary field int TaskFacade

--- app/src/main/java/io/github/mat3e/task/TaskFacade.java
+++ app/src/main/java/io/github/mat3e/task/TaskFacade.java
@@ -10,13 +10,11 @@ import static java.util.stream.Collectors.toList;
 
 public class TaskFacade {
     private final TaskRepository taskRepository;
-    private final TaskQueryRepository taskQueryRepository;
     private final TaskFactory taskFactory;
 
-    TaskFacade(final TaskFactory taskFactory, final TaskRepository taskRepository, final TaskQueryRepository taskQueryRepository) {
+    TaskFacade(final TaskFactory taskFactory, final TaskRepository taskRepository) {
         this.taskFactory = taskFactory;
         this.taskRepository = taskRepository;
-        this.taskQueryRepository = taskQueryRepository;
     }
 
     public List<TaskDto> saveAll(final List<TaskDto> tasks, SimpleProjectQueryEntity project) {
