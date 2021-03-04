307310ad480779d24cf9dedbe507852a415a3517
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 23:50:07 +0100

    Changed Task constructor to be private with all fields

--- domain/src/main/java/io/github/mat3e/task/Task.java
+++ domain/src/main/java/io/github/mat3e/task/Task.java
@@ -5,17 +5,22 @@ import io.github.mat3e.project.dto.SimpleProject;
 import java.time.ZonedDateTime;
 
 class Task {
-    private int id;
-    private String description;
-    private boolean done;
-    private ZonedDateTime deadline;
-    private int changesCount;
-    private String additionalComment;
+
+    private final int id;
+    private final String description;
+    private final boolean done;
+    private final ZonedDateTime deadline;
+    private final int changesCount;
+    private final String additionalComment;
     private final SimpleProject project;
 
-    Task(final String description, final ZonedDateTime deadline, final SimpleProject project) {
+    private Task(final int id, final String description, final boolean done, final ZonedDateTime deadline, final int changesCount, final String additionalComment, final SimpleProject project) {
+        this.id = id;
         this.description = description;
+        this.done = done;
         this.deadline = deadline;
+        this.changesCount = changesCount;
+        this.additionalComment = additionalComment;
         this.project = project;
     }
 
