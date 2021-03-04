5a3e3b88122bd4881f67aa4b8915b1018799a89a
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 14:31:51 +0100

    Removed all annotation from SimpleProject

--- domain/src/main/java/io/github/mat3e/project/dto/SimpleProject.java
+++ domain/src/main/java/io/github/mat3e/project/dto/SimpleProject.java
@@ -1,24 +1,9 @@
 package io.github.mat3e.project.dto;
 
-import org.springframework.data.annotation.PersistenceConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import static javax.persistence.GenerationType.IDENTITY;
-
-@Entity
-@Table(name = "projects")
 public class SimpleProject {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private int id;
-    private String name;
 
-    @PersistenceConstructor
-    protected SimpleProject() {}
+    private final int id;
+    private final String name;
 
     public SimpleProject(final int id, final String name) {
         this.id = id;
