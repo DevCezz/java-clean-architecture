baf4ffb7c3563d67b9e713642ef84541a53c647f
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 23:47:28 +0100

    Added fields and getters in SimpleProjectSnapshot

--- domain/src/main/java/io/github/mat3e/project/dto/SimpleProjectSnapshot.java
+++ domain/src/main/java/io/github/mat3e/project/dto/SimpleProjectSnapshot.java
@@ -1,4 +1,20 @@
 package io.github.mat3e.project.dto;
 
 public class SimpleProjectSnapshot {
+
+    private final int id;
+    private final String name;
+
+    SimpleProjectSnapshot(final int id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    String getName() {
+        return name;
+    }
 }
