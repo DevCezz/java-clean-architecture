02fcb427f8f746853e7ae5b3c1ebe360f3b73863
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 21:02:17 +0100

    Added implementation of ProjectStepRepository

--- adapters/src/main/java/io/github/mat3e/project/SqlRepositories.java
+++ adapters/src/main/java/io/github/mat3e/project/SqlRepositories.java
@@ -31,7 +31,24 @@ class ProjectRepositoryImpl implements ProjectRepository {
     }
 }
 
-interface SqlProjectStepRepository extends ProjectStepRepository, Repository<SqlProjectStep, Integer> {
+interface SqlProjectStepRepository extends Repository<SqlProjectStep, Integer> {
+
+    void delete(SqlProjectStep entity);
+}
+
+@org.springframework.stereotype.Repository
+class ProjectStepRepositoryImpl implements ProjectStepRepository {
+
+    private final SqlProjectStepRepository sqlProjectStepRepository;
+
+    ProjectStepRepositoryImpl(final SqlProjectStepRepository sqlProjectStepRepository) {
+        this.sqlProjectStepRepository = sqlProjectStepRepository;
+    }
+
+    @Override
+    public void delete(final ProjectStep entity) {
+        sqlProjectStepRepository.delete(SqlProjectStep.from(entity));
+    }
 }
 
 interface SqlProjectQueryRepository extends ProjectQueryRepository, Repository<SqlProject, Integer> {
