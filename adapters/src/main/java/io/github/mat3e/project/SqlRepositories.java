2dbcb916d13253dc57dd3d0e8895cc68e24ee48f
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 20:56:45 +0100

    Added implementation of ProjectRepository

--- adapters/src/main/java/io/github/mat3e/project/SqlRepositories.java
+++ adapters/src/main/java/io/github/mat3e/project/SqlRepositories.java
@@ -2,7 +2,33 @@ package io.github.mat3e.project;
 
 import org.springframework.data.repository.Repository;
 
-interface SqlProjectRepository extends ProjectRepository, Repository<SqlProject, Integer> {
+import java.util.Optional;
+
+interface SqlProjectRepository extends Repository<SqlProject, Integer> {
+
+    Optional<SqlProject> findById(Integer id);
+
+    Project save(SqlProject entity);
+}
+
+@org.springframework.stereotype.Repository
+class ProjectRepositoryImpl implements ProjectRepository {
+
+    private final SqlProjectRepository sqlProjectRepository;
+
+    ProjectRepositoryImpl(final SqlProjectRepository sqlProjectRepository) {
+        this.sqlProjectRepository = sqlProjectRepository;
+    }
+
+    @Override
+    public Optional<Project> findById(final Integer id) {
+        return sqlProjectRepository.findById(id).map(SqlProject::toProject);
+    }
+
+    @Override
+    public Project save(final Project entity) {
+        return sqlProjectRepository.save(SqlProject.from(entity));
+    }
 }
 
 interface SqlProjectStepRepository extends ProjectStepRepository, Repository<SqlProjectStep, Integer> {
