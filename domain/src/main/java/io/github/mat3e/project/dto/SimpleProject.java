97ee47244de1608cb37630fd3545e3f8114c074d
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 23:48:06 +0100

    Added restore method to get SimpleProject from snapshot

--- domain/src/main/java/io/github/mat3e/project/dto/SimpleProject.java
+++ domain/src/main/java/io/github/mat3e/project/dto/SimpleProject.java
@@ -2,6 +2,10 @@ package io.github.mat3e.project.dto;
 
 public class SimpleProject {
 
+    public static SimpleProject restore(SimpleProjectSnapshot snapshot) {
+        return new SimpleProject(snapshot.getId(), snapshot.getName());
+    }
+
     private final int id;
     private final String name;
 
