e6ddaf94ca09401c48549993d1f678e9e2ba2f42
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 14:59:01 +0100

    Added class SqlSimpleProject for using it in repo

---
+++ adapters/src/main/java/io/github/mat3e/project/SqlSimpleProject.java
@@ -0,0 +1,35 @@
+package io.github.mat3e.project;
+
+import org.springframework.data.annotation.PersistenceConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "projects")
+public class SqlSimpleProject {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+    private String name;
+
+    @PersistenceConstructor
+    protected SqlSimpleProject() {}
+
+    public SqlSimpleProject(final int id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
