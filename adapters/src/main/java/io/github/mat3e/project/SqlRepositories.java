b05672101a67f4d06218ed75b7603b8199ba596e
Cezary Sanecki <devcezz@gmail.com>
2021-03-04 15:04:42 +0100

    Used proper classes in project repositories

--- adapters/src/main/java/io/github/mat3e/project/SqlRepositories.java
+++ adapters/src/main/java/io/github/mat3e/project/SqlRepositories.java
@@ -2,11 +2,11 @@ package io.github.mat3e.project;
 
 import org.springframework.data.repository.Repository;
 
-interface SqlProjectRepository extends ProjectRepository, Repository<Project, Integer> {
+interface SqlProjectRepository extends ProjectRepository, Repository<SqlProject, Integer> {
 }
 
-interface SqlProjectStepRepository extends ProjectStepRepository, Repository<ProjectStep, Integer> {
+interface SqlProjectStepRepository extends ProjectStepRepository, Repository<SqlProjectStep, Integer> {
 }
 
-interface SqlProjectQueryRepository extends ProjectQueryRepository, Repository<Project, Integer> {
+interface SqlProjectQueryRepository extends ProjectQueryRepository, Repository<SqlProject, Integer> {
 }
