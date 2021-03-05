78a61160518e3249d347486213047e7bf7ad0c38
Cezary Sanecki <devcezz@gmail.com>
2021-03-05 23:14:31 +0100

    Added value object TaskSourceId

---
+++ domain/src/main/java/io/github/mat3e/task/vo/TaskSourceId.java
@@ -0,0 +1,15 @@
+package io.github.mat3e.task.vo;
+
+public class TaskSourceId {
+    private String id;
+
+    protected TaskSourceId() {}
+
+    public TaskSourceId(final String id) {
+        this.id = id;
+    }
+
+    String getId() {
+        return id;
+    }
+}
