b9a62514aaba20f2d005aa8cfa7c01fc8a9e792b
Cezary Sanecki <devcezz@gmail.com>
2021-03-05 14:22:29 +0100

    Added inner static class Step in Project

--- domain/src/main/java/io/github/mat3e/project/Project.java
+++ domain/src/main/java/io/github/mat3e/project/Project.java
@@ -11,16 +11,16 @@ class Project {
                 snapshot.getId(),
                 snapshot.getName(),
                 snapshot.getSteps().stream()
-                        .map(ProjectStep::restore)
+                        .map(Step::restore)
                         .collect(Collectors.toSet())
         );
     }
 
     private int id;
     private String name;
-    private final Set<ProjectStep> steps = new HashSet<>();
+    private final Set<Step> steps = new HashSet<>();
 
-    private Project(final int id, final String name, final Set<ProjectStep> steps) {
+    private Project(final int id, final String name, final Set<Step> steps) {
         this.id = id;
         this.name = name;
         steps.forEach(this::addStep);
@@ -42,11 +42,11 @@ class Project {
         this.name = name;
     }
 
-    public Set<ProjectStep> getSteps() {
+    public Set<Step> getSteps() {
         return steps;
     }
 
-    void addStep(ProjectStep step) {
+    void addStep(Step step) {
         if (steps.contains(step)) {
             return;
         }
@@ -54,7 +54,7 @@ class Project {
         step.setProject(this);
     }
 
-    void removeStep(ProjectStep step) {
+    void removeStep(Step step) {
         if (!steps.contains(step)) {
             return;
         }
@@ -63,6 +63,60 @@ class Project {
     }
 
     ProjectSnapshot getSnapshot() {
-        return new ProjectSnapshot(id, name, steps.stream().map(ProjectStep::getSnapshot).collect(Collectors.toSet()));
+        return new ProjectSnapshot(id, name, steps.stream().map(Step::getSnapshot).collect(Collectors.toSet()));
+    }
+
+    static class Step {
+
+        static Step restore(ProjectStepSnapshot snapshot) {
+            return new Step(snapshot.getId(), snapshot.getDescription(), snapshot.getDaysToProjectDeadline());
+        }
+
+        private int id;
+        private String description;
+        private int daysToProjectDeadline;
+        private Project project;
+
+        private Step(final int id, final String description, final int daysToProjectDeadline) {
+            this.id = id;
+            this.description = description;
+            this.daysToProjectDeadline = daysToProjectDeadline;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        void setId(int id) {
+            this.id = id;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        void setDescription(String description) {
+            this.description = description;
+        }
+
+        public int getDaysToProjectDeadline() {
+            return daysToProjectDeadline;
+        }
+
+        void setDaysToProjectDeadline(int daysToProjectDeadline) {
+            this.daysToProjectDeadline = daysToProjectDeadline;
+        }
+
+        public Project getProject() {
+            return project;
+        }
+
+        void setProject(Project project) {
+            this.project = project;
+        }
+
+        ProjectStepSnapshot getSnapshot() {
+            return new ProjectStepSnapshot(id, description, daysToProjectDeadline);
+        }
     }
 }
