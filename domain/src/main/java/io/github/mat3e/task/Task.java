21c2faa30e3862bb4db445e029f6b1a61aeff688
Cezary Sanecki <devcezz@gmail.com>
2021-03-05 23:34:42 +0100

    Added static method to create Task from TaskCreator

--- domain/src/main/java/io/github/mat3e/task/Task.java
+++ domain/src/main/java/io/github/mat3e/task/Task.java
@@ -1,6 +1,7 @@
 package io.github.mat3e.task;
 
 import io.github.mat3e.project.dto.SimpleProject;
+import io.github.mat3e.task.vo.TaskCreator;
 
 import java.time.ZonedDateTime;
 
@@ -15,6 +16,14 @@ class Task {
                 snapshot.getChangesCount(),
                 snapshot.getAdditionalComment(),
                 snapshot.getProject() != null ? SimpleProject.restore(snapshot.getProject()) : null
+        );
+    }
+
+    static Task createFrom(final TaskCreator source) {
+        return new Task(
+                source.getDescription(),
+                source.getDeadline(),
+                source.getId()
         );
     }
 
