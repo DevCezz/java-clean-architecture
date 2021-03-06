bbb7830a3e1b002f758895369af31b5391afbdb7
Cezary Sanecki <devcezz@gmail.com>
2021-03-06 14:36:53 +0100

    Injected DomainEventPublisher into TaskFacade bean

--- adapters/src/main/java/io/github/mat3e/task/TaskConfiguration.java
+++ adapters/src/main/java/io/github/mat3e/task/TaskConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.mat3e.task;
 
+import io.github.mat3e.DomainEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +9,13 @@ class TaskConfiguration {
 
     @Bean
     TaskFacade taskFacade(
-            final TaskRepository taskRepository
+            final TaskRepository taskRepository,
+            final DomainEventPublisher publisher
     ) {
         return new TaskFacade(
                 new TaskFactory(),
-                taskRepository);
+                taskRepository,
+                publisher
+        );
     }
 }
