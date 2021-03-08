24d9ac7f60005fd2c3b6ea022ce84535f660ea1a
Cezary Sanecki <devcezz@gmail.com>
2021-03-08 13:34:27 +0100

    Tested restore method of Project class

--- domain/src/test/io/github/mat3e/project/ProjectTest.groovy
+++ domain/src/test/io/github/mat3e/project/ProjectTest.groovy
@@ -1,6 +1,20 @@
 package io.github.mat3e.project
 
+
 import spock.lang.Specification
 
 class ProjectTest extends Specification {
+
+    def "should restore project from project snapshot"() {
+        given:
+            def snapshot = ProjectFixture.projectSnapshotWith3Steps()
+
+        when:
+            def result = Project.restore(snapshot).snapshot
+
+        then:
+            result.id == snapshot.id
+            result.name == snapshot.name
+            result.steps.size() == 3
+    }
 }
