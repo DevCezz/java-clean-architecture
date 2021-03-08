26fee3eca5b80cf9c120c212403ebbae56a3477e
Cezary Sanecki <devcezz@gmail.com>
2021-03-08 13:31:26 +0100

    Added class with methods to create test task snapshots

---
+++ domain/src/test/io/github/mat3e/project/ProjectFixture.java
@@ -0,0 +1,18 @@
+package io.github.mat3e.project;
+
+import java.util.Set;
+
+class ProjectFixture {
+
+    static ProjectSnapshot projectSnapshot() {
+        return new ProjectSnapshot(95, "first", projectStepSnapshots());
+    }
+
+    private static Set<ProjectStepSnapshot> projectStepSnapshots() {
+        return Set.of(
+                new ProjectStepSnapshot(1, "1", -1, false, false),
+                new ProjectStepSnapshot(2, "2", -2, false, false),
+                new ProjectStepSnapshot(3, "3", -3, false, false)
+        );
+    }
+}
