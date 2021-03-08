e58eff34c9a131cc7c2a6f57d4e8c5e9e75bb6cf
Cezary Sanecki <devcezz@gmail.com>
2021-03-08 12:00:49 +0100

    Used taskSnapshot method in TaskTest

--- domain/src/test/io/github/mat3e/task/TaskTest.groovy
+++ domain/src/test/io/github/mat3e/task/TaskTest.groovy
@@ -1,24 +1,13 @@
 package io.github.mat3e.task
 
-import io.github.mat3e.task.vo.TaskSourceId
-import spock.lang.Specification
 
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import spock.lang.Specification
 
 class TaskTest extends Specification {
 
     def "should restore task from task snapshot"() {
         given:
-            def snapshot = new TaskSnapshot(
-                    12,
-                    "easy task",
-                    true,
-                    ZonedDateTime.of(2002, 10, 12, 12, 23, 45, 10, ZoneId.of("Europe/Warsaw")),
-                    5,
-                    "do alone",
-                    new TaskSourceId("90")
-            )
+            def snapshot = TaskFixture.taskSnapshot()
 
         when:
             def result = Task.restore(snapshot).getSnapshot()
