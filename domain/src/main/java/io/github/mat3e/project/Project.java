5a24c73e0a6a0696c17a80db903964ace30218ef
Cezary Sanecki <devcezz@gmail.com>
2021-03-08 14:27:47 +0100

    Added equals and hashCode to Project - by id

--- domain/src/main/java/io/github/mat3e/project/Project.java
+++ domain/src/main/java/io/github/mat3e/project/Project.java
@@ -5,6 +5,7 @@ import io.github.mat3e.task.vo.TaskSourceId;
 
 import java.time.ZonedDateTime;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
@@ -144,6 +145,19 @@ class Project {
                     hasCorrespondingTask,
                     isCorrespondingTaskDone
             );
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            final Step step = (Step) o;
+            return id == step.id;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
         }
     }
 }
