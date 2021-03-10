78ccb585bef2989c1793cf271698046c9a55fe06
Cezary Sanecki <devcezz@gmail.com>
2021-03-10 21:55:41 +0100

    Added required fields to Test ProjectInitializer

--- app/src/test/groovy/io/github/mat3e/project/ProjectInitializerTest.groovy
+++ app/src/test/groovy/io/github/mat3e/project/ProjectInitializerTest.groovy
@@ -1,6 +1,14 @@
 package io.github.mat3e.project
 
+
 import spock.lang.Specification
+import spock.lang.Subject
 
 class ProjectInitializerTest extends Specification {
+
+    def repository = Mock(ProjectRepository)
+    def queryRepository = Mock(ProjectQueryRepository)
+
+    @Subject
+    def initializer = new ProjectInitializer(repository, queryRepository)
 }
