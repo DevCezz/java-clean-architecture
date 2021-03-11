58d88791e4542aa394af3b4bdd74ad76114f3c8d
Cezary Sanecki <devcezz@gmail.com>
2021-03-11 09:34:01 +0100

    Initilized test fields

--- app/src/test/groovy/io/github/mat3e/project/ProjectFacadeTest.groovy
+++ app/src/test/groovy/io/github/mat3e/project/ProjectFacadeTest.groovy
@@ -1,6 +1,38 @@
 package io.github.mat3e.project
 
+import io.github.mat3e.task.TaskFacade
 import spock.lang.Specification
+import spock.lang.Subject
 
 class ProjectFacadeTest extends Specification {
+
+    def factory = new ProjectFactory()
+    def repository = new ProjectRepositoryImpl()
+    def taskFacade = Mock(TaskFacade)
+
+    @Subject
+    def facade = new ProjectFacade(factory, repository, taskFacade)
+}
+
+class ProjectRepositoryImpl implements ProjectRepository {
+
+    @Override
+    Optional<Project> findById(final Integer id) {
+        return null
+    }
+
+    @Override
+    Optional<Project> findByNestedStepId(final Integer id) {
+        return null
+    }
+
+    @Override
+    Project save(final Project entity) {
+        return null
+    }
+
+    @Override
+    void delete(final Project.Step entity) {
+
+    }
 }
