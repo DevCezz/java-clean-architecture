e33e615e80f08df13a5a9334cf0a1a95d3f31a30
Cezary Sanecki <devcezz@gmail.com>
2021-03-11 13:07:58 +0100

    Changed signatures for method in ProjectFixture

--- app/src/test/groovy/io/github/mat3e/project/ProjectFixture.java
+++ app/src/test/groovy/io/github/mat3e/project/ProjectFixture.java
@@ -4,13 +4,13 @@ import static java.util.Collections.singletonList;
 
 class ProjectFixture {
 
-    static Project projectWithStepWithId(int id) {
+    static Project projectWithStepUndoneTaskWithId(int id) {
         return Project.restore(
-                new ProjectSnapshot(10, "20", singletonList(stepWithId(id).getSnapshot()))
+                new ProjectSnapshot(10, "20", singletonList(stepUndoneTaskWithId(id).getSnapshot()))
         );
     }
 
-    static Project.Step stepWithId(int id) {
+    static Project.Step stepUndoneTaskWithId(int id) {
         return Project.Step.restore(
                 new ProjectStepSnapshot(id, "desc", -2, true, false)
         );
