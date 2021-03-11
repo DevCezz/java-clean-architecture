71bcd6ebfa0c847e7de99aaa36010e8ddf2f2b90
Cezary Sanecki <devcezz@gmail.com>
2021-03-11 10:20:30 +0100

    Implemented repository for testing ProjectFacade

--- app/src/test/groovy/io/github/mat3e/project/ProjectFacadeTest.groovy
+++ app/src/test/groovy/io/github/mat3e/project/ProjectFacadeTest.groovy
@@ -1,5 +1,6 @@
 package io.github.mat3e.project
 
+
 import io.github.mat3e.task.TaskFacade
 import spock.lang.Specification
 import spock.lang.Subject
@@ -16,23 +17,34 @@ class ProjectFacadeTest extends Specification {
 
 class ProjectRepositoryImpl implements ProjectRepository {
 
+    private int nextId = 1
+    private Map<Integer, Project> database = new HashMap<>()
+
     @Override
     Optional<Project> findById(final Integer id) {
-        return null
+        return Optional.ofNullable(database.get(id))
     }
 
     @Override
     Optional<Project> findByNestedStepId(final Integer id) {
-        return null
+        return database.values().stream()
+            .filter(project -> project.snapshot.steps.stream()
+                    .anyMatch(step -> step.id == id)
+            ).findFirst()
     }
 
     @Override
     Project save(final Project entity) {
-        return null
+        def project = findById(entity.snapshot.id)
+                .map(task -> entity)
+                .orElseGet(() -> Project.restore(new ProjectSnapshot(nextId++, entity.snapshot)))
+        database.put(project.snapshot.id, project)
+        return project
     }
 
     @Override
     void delete(final Project.Step entity) {
-
+        findByNestedStepId(entity.snapshot.id)
+            .ifPresent(project -> project.removeStep(entity.snapshot.id))
     }
 }
