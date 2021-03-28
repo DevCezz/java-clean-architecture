faa30e01e5840c155080aee68f7f0d93db7e8697
Cezary Sanecki <devcezz@gmail.com>
2021-03-28 20:28:01 +0200

    Added integration test for ProjectController

---
+++ adapters/src/test/java/io/github/mat3e/project/ProjectControllerTest.java
@@ -0,0 +1,31 @@
+package io.github.mat3e.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class ProjectControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void should_list_all_projects() throws Exception {
+        mockMvc.perform(get("/projects"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(1)))
+                .andExpect(jsonPath("$[*].steps.*", hasSize(3)));
+    }
+}
