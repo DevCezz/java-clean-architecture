784a78584f9605a5c33d0bbebc4e1f655ffa5587
Cezary Sanecki <devcezz@gmail.com>
2021-03-31 00:14:16 +0200

    Made security be enabled when there is not test profile

--- adapters/src/main/java/io/github/mat3e/auth/SecurityConfiguration.java
+++ adapters/src/main/java/io/github/mat3e/auth/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package io.github.mat3e.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,7 @@ import org.springframework.security.web.authentication.AnonymousAuthenticationFi
 import java.util.Set;
 
 @EnableWebSecurity
+@Profile("!test")
 class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final TokenService tokenService;
 
